e65960697d8cc552581f27cf25b459f0b1314bb2
cahartsell <charles.a.hartsell@vanderbilt.edu>
2016-12-01 16:24:53 -0600

    Revised pathfinder.m to use variant of A* algorithm

--- pathfinder.m
+++ pathfinder.m
@@ -38,7 +38,7 @@ function [ desired_heading ] = pathfinder( current_map, robot_pos, target, SAVE_
     points_ind(2,:,:) = points(2, :, :) - map_origin(2);
     points_ind = ceil( points_ind ./ TILE_SIZE );
     
-    % Add gradient weighting to map which decreases with distance from target
+    % Add gradient weighting to map which increases with distance from target
     dx2 = repmat( (map_origin(1)+TILE_SIZE/2) , 1, MAP_SIZE) + (TILE_SIZE * (0:MAP_SIZE-1));
     dy2 = repmat( (map_origin(2)+TILE_SIZE/2) , 1, MAP_SIZE) + (TILE_SIZE * (0:MAP_SIZE-1));
     dx2 = dx2 - repmat( target(1), 1, MAP_SIZE );
@@ -47,30 +47,28 @@ function [ desired_heading ] = pathfinder( current_map, robot_pos, target, SAVE_
     dy2 = dy2.^2;
     dist_from_target = repmat( dx2, MAP_SIZE, 1)' + repmat( dy2, MAP_SIZE, 1);
     dist_from_target = dist_from_target.^(1/2);
-    denom_check = dist_from_target < 0.1;     % Prevent divide by zero
-    dist_from_target = dist_from_target + (denom_check/10);
-    map = (dist_from_target.^(-1)) * DIST_WEIGHT;
+    map = dist_from_target ./  TILE_SIZE;
     
-    % Give negative weighting to map tiles with obstacles (walls)
+    % Give infinite weighting to map tiles with obstacles (walls)
     % Not vectorized. For loop is slow, but easy
     % Check that indicies are within range
     temp = points_ind > 0;
     points_ind = points_ind .* temp;
     temp = points_ind <= MAP_SIZE;
     points_ind = points_ind .* temp;
-    for j = 1:num_walls
-        last_x = points_ind(1,1,j);
-        last_y = points_ind(2,1,j);
-        for k = 1:max_num_points
-            x_ind = points_ind(1,k,j);
-            y_ind = points_ind(2,k,j);
+    for i = 1:num_walls
+        last_x = points_ind(1,1,i);
+        last_y = points_ind(2,1,i);
+        for j = 1:max_num_points
+            x_ind = points_ind(1,j,i);
+            y_ind = points_ind(2,j,i);
             if (x_ind > 0) && (y_ind > 0)
-                map( x_ind, y_ind ) = -1;
+                map( x_ind, y_ind ) = inf;
                 % Simple (but not particularly precise) method to prevent
                 % diagonal jumps through walls
                 if (last_x > 0) && (last_y > 0) && (x_ind ~= last_x) && (y_ind ~= last_y)
-                    map( last_x, y_ind ) = -1;
-                    map( x_ind, last_y ) = -1;
+                    map( last_x, y_ind ) = inf;
+                    map( x_ind, last_y ) = inf;
                 end
                 last_x = x_ind;
                 last_y = y_ind;
@@ -81,68 +79,122 @@ function [ desired_heading ] = pathfinder( current_map, robot_pos, target, SAVE_
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %                       PATHFINDING                         %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Find best path using custom variant of Dijkstra's algorithm
-    % A* algorithm would be faster, but harder to implement
+    % Find best path using a variant of A* algorithm
+    % Website used for reference:
     % http://www.redblobgames.com/pathfinding/a-star/introduction.html
+    
+    % If target is in range, set as goal
+    % Otherwise, set goal to lowest value point in map
+    target_ind(1) = target(1) - map_origin(1);
+    target_ind(2) = target(2) - map_origin(2);
+    target_ind = ceil( target_ind ./ TILE_SIZE );
+    if (target_ind(1) > 0) && (target_ind(1) <= MAP_SIZE) && (target_ind(2) > 0) && (target_ind(2) <= MAP_SIZE)
+        goal_index = target_ind;
+    else
+        [~, temp_index] = min( map(:) );
+        goal_index(2) = ceil( temp_index/MAP_SIZE );
+        goal_index(1) = mod( temp_index, MAP_SIZE );
+        if (goal_index(1) == 0)
+            goal_index(1) = MAP_SIZE;
+        end
+    end
+    
     num_added = 0;
-    last_frontier(1,1) = ceil(MAP_SIZE/2);
-    last_frontier(2,1) = ceil(MAP_SIZE/2);
+    found_goal = 0;
     came_from = zeros(2, MAP_SIZE, MAP_SIZE);
-    while ( last_frontier(1,1) ) % Run till frontier fully explored
-        frontier = zeros(2,1); % Clear frontier variable
-        [~, sz] = size( last_frontier );
-        for i = 1:sz        
-            cur_x_ind = last_frontier(1, i);
-            cur_y_ind = last_frontier(2, i);
+    cost_so_far = zeros(MAP_SIZE, MAP_SIZE);
+    map_frontier = zeros(3, 1000);
+    % map_frontier stores x and y coords and a priority value of all points
+    % in frontier. Frontier made large to prevent growth every loop iteration.
+    % MATLAB does not have built in data queue structure, or any
+    % particularly efficient way (known to me) to maintain a queue. Instead, 
+    % maintain a count of empty cells, and append new entries to 
+    % map_frontier when necessary
+    map_frontier(3,:) = inf; % Minimum priority
+    map_frontier(1,1) = ceil( MAP_SIZE/2 );
+    map_frontier(2,1) = ceil( MAP_SIZE/2 );
+    map_frontier(3,1) = 0; % Maximum priority
+    size_frontier = 1;
+    num_empty_cells = 0;
+    while (size_frontier > 0) && (found_goal == 0) % Run till frontier is empty or goal is found
+        [~, cur_frontier_index] = min( map_frontier(3, :) ); % Find highest priority
+        cur_x_index = map_frontier(1, cur_frontier_index);
+        cur_y_index = map_frontier(2, cur_frontier_index);
+        
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%% DEBUG
+%         temp_map = map;
+%         temp_ind = find( map_frontier(3,:) < inf );
+%         [~, sz] = size( temp_ind );
+%         for i = 1:sz
+%             temp_map( map_frontier(1,temp_ind(i)), map_frontier(2,temp_ind(i)) ) = 0;
+%         end
+%         im = imagesc( [0, MAP_SIZE], [0, MAP_SIZE], temp_map' );
+%         pause(0.01);
+%         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%   
+        
+        size_frontier = size_frontier - 1;
+        for i = -1:1:1
             for j = -1:1:1
-                for k = -1:1:1
-                    % Nested for loops allow movement in 4 directions (Forward,
-                    % Back, Left, Right, Diagonal)
-                    if (j == 0) && (k == 0) % Don't check current pos
-                        continue;
+                % Nested for loops allow movement in 8 directions (Forward,
+                % Back, Left, Right, and Diagonals)
+                if (i == 0) && (j == 0) % Don't check current pos
+                    continue;
+                end
+                if (i ~= 0) && (j ~= 0)
+                    next_cost = 1.414; % Correct distance cost for diagonal steps
+                else
+                    next_cost = 1;
+                end
+                next_x_ind = cur_x_index + i;
+                next_y_ind = cur_y_index + j;
+                if (next_x_ind < 1) || (next_x_ind > MAP_SIZE) || (next_y_ind < 1) || (next_y_ind > MAP_SIZE)
+                    continue; % Skip if out of map range
+                end
+                if ~(map( next_x_ind, next_y_ind ) < inf) 
+                    continue; % Skip if obstacle in the way
+                end
+                new_cost = cost_so_far(cur_x_index, cur_y_index) + next_cost;
+                % No 'came_from' data on tile means tile unvisited
+                old_tile = came_from(1, next_x_ind, next_y_ind);
+                if (~old_tile) || (new_cost < cost_so_far( next_x_ind, next_y_ind ))
+                    % Tile not visited before, or found shorter route
+                    if (num_added == 0)
+                        temp_frontier_index = cur_frontier_index; % Replace current entry
+                    else
+                        temp_frontier_index = size_frontier + 1 + num_empty_cells; % append to end
                     end
-                    next_x_ind = cur_x_ind + j;
-                    next_y_ind = cur_y_ind + k;
-                    if (next_x_ind < 1) || (next_x_ind > MAP_SIZE) || (next_y_ind < 1) || (next_y_ind > MAP_SIZE)
-                        continue; % Skip if out of map range
-                    end
-                    % No 'came_from' data on tile means tile unvisited
-                    old_tile = came_from(1, next_x_ind, next_y_ind);
-                    if (map( next_x_ind, next_y_ind ) > 0) && (~old_tile) 
-                        % No obstacle in tile and tile not visited before
-                        % frontier grows every loop iteration (inefficient)
-                        frontier(1, num_added+1) = next_x_ind;
-                        frontier(2, num_added+1) = next_y_ind;
-                        came_from(1, next_x_ind, next_y_ind) = cur_x_ind;
-                        came_from(2, next_x_ind, next_y_ind) = cur_y_ind;
-                        num_added = num_added + 1;
-                    end
+                    cost_so_far( next_x_ind, next_y_ind ) = new_cost;
+                    map_frontier(1, temp_frontier_index) = next_x_ind;
+                    map_frontier(2, temp_frontier_index) = next_y_ind;
+                    map_frontier(3, temp_frontier_index) = new_cost + map(next_x_ind, next_y_ind);
+                    came_from(1, next_x_ind, next_y_ind) = cur_x_index;
+                    came_from(2, next_x_ind, next_y_ind) = cur_y_index;
+                    num_added = num_added + 1;
+                    size_frontier = size_frontier + 1;
+                end
+                if (next_x_ind == goal_index(1)) && (next_y_ind == goal_index(2))
+                    found_goal = 1;
                 end
             end
         end
-        num_added = 0;
-        
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%% DEBUG
-        temp_map = map;
-        for i = 1:sz
-            temp_map( last_frontier(1,i), last_frontier(2,i) ) = 0;
+        if (num_added == 0) % No replacement found for current frontier. Incremement empty cell count
+            num_empty_cells = num_empty_cells + 1;
+            map_frontier(:, cur_frontier_index) = 0;
+            map_frontier(3, cur_frontier_index) = inf;
         end
-        im = imagesc( temp_map' );
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-        last_frontier = frontier;
-        
+        num_added = 0;
     end
     
-    % Find highest value point within the allowed_map
-    allowed_tile_mask(:,:) = came_from(1,:,:) > 0;
-    allowed_map = map .* allowed_tile_mask;
-    [~, temp_index] = max( allowed_map(:) );
-    goal_index(2) = ceil( temp_index/MAP_SIZE );
-    goal_index(1) = mod( temp_index, MAP_SIZE );
-    if (goal_index(1) == 0)
-        goal_index(1) = MAP_SIZE;
+    if( found_goal == 0 )
+        % If goal not found, find best obtainable point
+        allowed_tile_mask(:,:) = came_from(1,:,:) > 0;
+        allowed_map = map ./ allowed_tile_mask; % MATLAB allows divide by 0. Returns 'inf'
+        [~, temp_index] = min( allowed_map(:) );
+        goal_index(2) = ceil( temp_index/MAP_SIZE );
+        goal_index(1) = mod( temp_index, MAP_SIZE );
+        if (goal_index(1) == 0)
+            goal_index(1) = MAP_SIZE;
+        end
     end
     
     % Create path by backtracking steps from goal to start
